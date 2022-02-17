fabd3125cb8f66bcef8362c2bcc8e92ffa75ad74
Nayan Gurung <nayangrg.se@gmail.com>
2022-02-17 13:48:42 +0000

    Create new exceptions

--- src/main/java/com/nayan/car/exception/CarNotFoundException.java
+++ src/main/java/com/nayan/car/exception/CarNotFoundException.java
@@ -1,4 +1,4 @@
-package com.nayan.car;
+package com.nayan.car.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
--- src/main/java/com/nayan/car/exception/RegNumberInvalid.java
+++ src/main/java/com/nayan/car/exception/RegNumberInvalid.java
@@ -1,12 +1,11 @@
-package com.nayan.car;
+package com.nayan.car.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.BAD_REQUEST)
-public class InvalidRequestException extends RuntimeException{
-    public InvalidRequestException(String message) {
+public class RegNumberInvalid extends RuntimeException{
+    public RegNumberInvalid(String message) {
         super(message);
     }
-
 }
