7b33333b2759a851d5030e31d2f3260a9bfeb634
Nayan Gurung <nayangrg.se@gmail.com>
2022-02-17 12:39:38 +0000

    Changed the parameters of deleteCar method

--- src/main/java/com/nayan/car/CarDAO.java
+++ src/main/java/com/nayan/car/CarDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 //CarDAO is an interface therefore it does not need any bean
 public interface CarDAO {
     int insertCar(Car car);
-    int deleteCar(Car car);
+    int deleteCar(Integer id);
     int updateCar(Integer id, Car updatedCar);
     Car selectCar(Integer id);
     List<Car> selectAllCars();
